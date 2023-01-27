41aa771369666bd0d146000a96d704e7d622b1dd
Nate Sales <nate@natesales.net>
2023-01-26 19:03:30 -0500

    chore: ignore cmake build resources

--- cmake-build-debug/imgui-cmake/libs/SDL/include/SDL3/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-prerelease-3.0.0-229-ga463aca0d (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-prerelease-3.0.0-229-ga463aca0d"
-#endif
